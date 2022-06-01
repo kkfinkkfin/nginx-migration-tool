f405f575d0aee923ab4b79bd17704b59e4d210c4
kkfinkkfin <754556014@qq.com>
2022-06-01 17:23:07 +0800

    updated README.md

--- main.go
+++ main.go
@@ -1,136 +1,183 @@
-package main
+# NGINX Ingress Controller Migration Tool
 
-import (
-	"context"
-	"encoding/json"
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"runtime"
+The NGINX Ingress Controller Migration Tool use for migration of the [Kubernetes ingress-nginx](https://kubernetes.github.io/ingress-nginx/) configurations yaml for to [Nginx Ingress Controller](https://github.com/nginxinc/kubernetes-ingress).
+ 
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
-	"k8s.io/klog"
-)
+## Overview
 
-var (
+The annotations of the [Kubernetes ingress-nginx](https://kubernetes.github.io/ingress-nginx/) uses that [Nginx Ingress Controller](https://github.com/nginxinc/kubernetes-ingress) do use or do it different. This tool help to automate migration of the configurations to yaml of [Nginx Ingress Controller](https://github.com/nginxinc/kubernetes-ingress).
 
-	//Set during build
-	version string
-	commit  string
-	date    string
+There is three option of migrating using NGINX Ingress Resources
 
-	versionFlag = flag.Bool("version", false, "Print the version, git-commit hash and build date and exit")
+1, Convert Ingress to Ingress or CRD
 
-	original_ingress_name      = flag.String("original_ingress_name", "test", "Define the original ingress")
-	new_ingress_class_name     = flag.String("new_ingress_classname", "nginx-plus", "Define the new ingress class")
-	ingress_namespace_name     = flag.String("namespace_name", "default", "Define the namespace")
-	new_ingress_namespace_name = flag.String("new_namespace_name", "default", "Define the namespace")
-	new_ingress_name           = flag.String("new_ingress_name", "test-migration-edtion", "Define the new ingress name")
-	//enable_megertable          = flag.Bool("enable_mgertable", false, "Define whether enable convert to Megertable Ingress Resource")
-	//enable_crd                 = flag.Bool("enable_crd", false, "Define whether convert all ingress to Customer Resource Definition")
-)
+•For functions that can be achieved through Ingress resource types and annotations, it is supported to convert to the same Ingress resource type and use NGINX annotations.
+•For advanced functions such as canary functionality, the NGINX Ingress Controller cannot be implemented through ingress with annotations, and is transformed into the resource type of CRD.
+•It is also supported, and all functions are converted to CRD resources.
 
-func main() {
-	flag.Parse()
+![image](https://user-images.githubusercontent.com/59547386/171353803-e8a68e20-dadc-4bd4-8134-6e22e3be94b0.png)
 
-	versionInfo := fmt.Sprintf("Version=%v GitCommit=%v Date=%v Arch=%v/%v", version, commit, date, runtime.GOOS, runtime.GOARCH)
-	if *versionFlag {
-		fmt.Println(versionInfo)
-		os.Exit(0)
-	}
 
-	var kubeconfig *string
-	ctx := context.Background()
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		klog.Fatal(err)
-		return
-	}
-	//klog.Fatal("config = ", config)
-	//create clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		klog.Fatal(err)
-		return
-	}
-	// var config *rest.Config
-	// ctx := context.Background()
+2, New Ingess/CRD resource with  specific IngressClass Name
 
-	//read config json into keyinfos map
-	f, err := ioutil.ReadFile("./cfg/ingresskey.json")
-	if err != nil {
-		fmt.Println("open file err = ", err)
-		return
-	}
+•Do not modify the existing Ingress resources in the cluster and would not affect the existing access traffic.
+•A new set of Ingress resources for NGINX ICs can be used with user-specified IngressClass names so that ICs that also declare the --ingress-class can watch to this set of new Ingress resources.
 
-	keyinfos := make(map[string]string)
+![image](https://user-images.githubusercontent.com/59547386/171353852-b4e9af0b-8ea4-4465-8e58-c8bcc01db4d0.png)
 
-	errr := json.Unmarshal([]byte(f), &keyinfos)
-	if errr != nil {
-		fmt.Println("Umarshal failed:", err)
-		return
-	}
 
-	//get Ingress resource
-	ingresslist, err := clientset.ExtensionsV1beta1().Ingresses(*ingress_namespace_name).List(ctx, metav1.ListOptions{})
-	if err != nil {
-		klog.Fatal(err)
-		return
-	}
-	//ingress annotation key
-	ingresses := ingresslist.Items
-	for _, ingress := range ingresses {
-		fmt.Printf("===== processing name %s ===== \n", ingress.Name)
-		if ingress.Name == *original_ingress_name {
-			newingress := ingress
-			newingress.Name = *new_ingress_name
-			newingress.Namespace = *new_ingress_namespace_name
-			newingress.ResourceVersion = ""
+3, New Mergeable Ingress resource with  specific IngressClass Name
 
-			err_delete := clientset.ExtensionsV1beta1().Ingresses(*new_ingress_namespace_name).Delete(ctx, newingress.Name, metav1.DeleteOptions{})
-			if err_delete == nil {
-				fmt.Printf("Name %s already exist, delete\n", newingress.Name)
-			}
+•Do not modify the existing Ingress resources in the cluster and would not affect the existing access traffic.
+•In the CE, there are multiple ingresses with the same hostname with different paths, and we can't convert directly due to host collision detection, so we need to identify them first and then convert them to Mergeable Ingress resources.
 
-			//replacing value
-			fmt.Println("replacing value:")
-			for key, value := range ingress.Annotations {
-				fmt.Printf("replace value %s \n", key)
-				switch key {
-				case "nginx.ingress.kubernetes.io/upstream-hash-by":
-					newingress.Annotations[key] = "hash " + value + " consistent"
-				}
-			}
+![image](https://user-images.githubusercontent.com/59547386/171353885-e84e4b68-4770-4721-8253-dfe9a795750c.png)
 
-			//replacing key
-			fmt.Println("replacing key:")
-			for key, value := range ingress.Annotations {
-				replace_key, ok := keyinfos[key]
-				if ok {
-					fmt.Printf("replace key %s to %s\n", key, replace_key)
-					delete(newingress.Annotations, key)
-					newingress.Annotations[replace_key] = value
-				}
-			}
 
-			fmt.Println("replacing ingressClassName")
-			newingress.Spec.IngressClassName = new_ingress_class_name
+## Showcase example
 
-			_, err_create := clientset.ExtensionsV1beta1().Ingresses("default").Create(ctx, &newingress, metav1.CreateOptions{})
-			if err_create != nil {
-				fmt.Printf("Encounter error in creating newingress %s\n", err_create)
-			}
-		}
-	}
-}
+![image](https://user-images.githubusercontent.com/59547386/171353909-e7818c5b-2d8c-4b53-a0a1-3ecf2547a3e2.png)
+
+
+![image](https://user-images.githubusercontent.com/59547386/171353953-ba03c0a3-fe66-457b-bc05-e2852e7c7cc6.png)
+
+
+
+## Getting Started
+
+In this section, we show how to quickly run NGINX Ingress Controller Migration tool.
+
+### A Note about NGINX Ingress Controller
+
+If you’d like to use the tool with [NGINX Ingress Controller](https://github.com/nginxinc/kubernetes-ingress/) for Kubernetes, see [this doc](https://docs.nginx.com/nginx-ingress-controller/) for the installation instructions.
+
+### Prerequisites
+
+We assume that you have already installed NGINX or NGINX Plus Ingress Controller. 
+
+## Usage
+
+### Command-line Arguments
+
+```
+Usage of ./nginx-migration-tool:
+  -original_ingress_name
+        Define the original ingress to be converted. Default name is "test" temporarily. 
+  -namespace_name
+        Defines the namespace in which the original inrgess to be converted is located. Default is the "default" namespace.
+  -new_namespace_name
+        Defines the namespace of the new ingrerss resource object after conversion. Default is the "default" namespace.
+  -new_ingress_classname
+        Defines the inress class name of the new ingrerss resource object after conversion. Default is "nginx-plus" temporarily. 
+  -new_ingress_name
+        Defines the inress name of the new ingrerss resource object after conversion. Default is "test-migration-edtion" temporarily. 
+  
+```
+
+## Different configuration
+
+### Advanced annotations in Ingress type with corresponding keys:
+Community Ingress Controller | NGINX Ingress Controller
+----|----|
+`nginx.ingress.kubernetes.io/app-root` | `nginx.org/server-snippets: "if($request_uri = '/'){ \n return 302 $http_x_forward_proto://$host{{ $location.Rewrite.AppRoot }}; \n }"` |
+`nginx.ingress.kubernetes.io/configuration-snippet` | `nginx.org/location-snippets` |
+`nginx.ingress.kubernetes.io/proxy-body-size` | `nginx.org/client-max-body-size` | 
+`nginx.ingress.kubernetes.io/proxy-connect-timeout` | `nginx.org/proxy-connect-timeout` | 
+`nginx.ingress.kubernetes.io/proxy-send-timeout` | `nginx.org/proxy-send-timeout` |
+`nginx.ingress.kubernetes.io/proxy-read-timeout` | `nginx.org/proxy-read-timeout` |
+`nginx.ingress.kubernetes.io/rewrite-target` | `nginx.org/rewrites` | 
+`nginx.ingress.kubernetes.io/server-snippet` | `nginx.org/server-snippets` | 
+`nginx.ingress.kubernetes.io/ssl-redirect`   | `ingress.kubernetes.io/ssl-redirect` | 
+`nginx.ingress.kubernetes.io/stream-snippet` | `nginx.org/stream-snippets` | 
+`nginx.ingress.kubernetes.io/upstream-hash-by: "$request_url"` | `nginx.org/lb-method: "hash $request_uri consistent"` | 
+`nginx.ingress.kubernetes.io/load-balance` | `nginx.org/lb-method` | 
+`nginx.ingress.kubernetes.io/proxy-buffering` | `nginx.org/proxy-buffering`|
+`nginx.ingress.kubernetes.io/proxy-buffers-number` | `nginx.org/proxy-buffers`|
+`nginx.ingress.kubernetes.io/proxy-buffer-size` | `nginx.org/proxy-buffer-size`|
+`nginx.ingress.kubernetes.io/proxy-max-temp-file-size` | `nginx.org/proxy-max-temp-file-size` | 
+
+#### Cookie
+Community Ingress Controller | NGINX Ingress Controller
+----|----|
+
+#### Redirect
+Community Ingress Controller | NGINX Ingress Controller
+----|----|
+
+### Advanced annotations in Ingress type with snippets:
+Community Ingress Controller | NGINX Ingress Controller
+----|----|
+`nginx.ingress.kubernetes.io/app-root` | `nginx.org/server-snippets: "if($request_uri = '/'){ \n return 302 $http_x_forward_proto://$host{{ $location.Rewrite.AppRoot }}; \n }"` |
+`nginx.ingress.kubernetes.io/client-body-buffer-size` | `"nginx.org/location-snippets": "client_body_buffer_size 30M; "` |
+`nginx.ingress.kubernetes.io/custom-http-errors: "code"` | `nginx.org/location-snippets: /| \n location / {\n   error_page 404 = @fallback;\n}` |
+`nginx.ingress.kubernetes.io/default-backend: "default-svc"` | `nginx.org/location-snippets: /| \n location @fallback {\n   proxy_pass http://backend;\n}` |
+`nginx.ingress.kubernetes.io/proxy-cookie-domain` | `nginx.org/location-snippets: proxy_cookie_domain off;` | 
+`nginx.ingress.kubernetes.io/proxy-cookie-path` | `nginx.org/location-snippets: proxy_cookie_path off;` | 
+
+#### Authentication
+Community Ingress Controller | NGINX Ingress Controller
+----|----|
+
+#### External Authentication
+Community Ingress Controller | NGINX Ingress Controller
+----|----|
+
+#### CORS
+Community Ingress Controller | NGINX Ingress Controller
+----|----|
+
+#### Rate Limiting
+Community Ingress Controller | NGINX Ingress Controller
+----|----|
+
+#### Mirror
+Community Ingress Controller | NGINX Ingress Controller
+----|----|
+
+### Advanced annotations in CRD type
+For the following scenarios, it is recommended to convert to CRD type resource objects, so that the configuration is more clearer than ingress.
+
+#### Canary
+Community Ingress Controller | NGINX Ingress Controller
+----|----|
+
+#### CORS
+Community Ingress Controller | NGINX Ingress Controller
+----|----|
+
+#### Rate Limiting
+Community Ingress Controller | NGINX Ingress Controller
+----|----|
+
+
+####Backend Certificate Authentication
+Community Ingress Controller | NGINX Ingress Controller
+----|----|
+
+
+### Megertable Ingresses
+
+### Other that can not be convert
+####ModSecurity
+Community Ingress Controller | NGINX Ingress Controller
+----|----|
+`nginx.ingress.kubernetes.io/enable-modsecurity` | - |
+`nginx.ingress.kubernetes.io/enable-owasp-core-rules` | - | 
+`nginx.ingress.kubernetes.io/modsecurity-transaction-id`| - |
+`nginx.ingress.kubernetes.io/modsecurity-snippet` | - | 
+
+
+
+## Troubleshooting
+
+The program logs errors to the standard output. When using Docker, if it doesn’t work as expected, check its logs using [docker logs](https://docs.docker.com/engine/reference/commandline/logs/) command.
+
+## Releases
+
+
+### Binaries
+
+
+## Building the Migration tool
+
+
